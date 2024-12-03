76bd42637910a480447c997566127138ea805e3a
JENNIFER974-DEL <jennifernyaguthii23@gmail.com>
2024-12-03 08:07:29 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 let num1 = 31;
 let num2 = 2;
-let num3 = 22;
-let num4 = 6;
-let multiply = num1 * num2; 
-let random = Math.floor(Math.random() * 100) + 1;
+let multiply = num1 * num2;
+
+console.log(multiply); 
+
+let random = Math.floor(Math.random() * 10);
+console.log(random);
+
+let num3 = 14;
+let num4 = 5;
 let mod = num3 % num4;
-let max = Math.max(1, 5, 10, 20); 
+
+console.log(mod); 
+
+let numbers = [10, 5, 20, 15, 8];
+let max = Math.max(...numbers);
+
+console.log(max); 
